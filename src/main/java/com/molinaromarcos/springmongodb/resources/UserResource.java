6160c0c7f30115e244de06f9bc4b085dbd537b8b
marcosmolinaro1 <marcos.molinaro@hotmail.com>
2020-07-08 22:13:44 -0300

    POST User insert

--- src/main/java/com/molinaromarcos/springmongodb/resources/UserResource.java
+++ src/main/java/com/molinaromarcos/springmongodb/resources/UserResource.java
@@ -1,7 +1,7 @@
 package com.molinaromarcos.springmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
-
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.molinaromarcos.springmongodb.domain.User;
 import com.molinaromarcos.springmongodb.dto.UserDTO;
@@ -36,10 +37,13 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(obj));	
 	}
 
-	/*@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){	
-		User obj = service.fromDTO(objDto);
+		User obj = service.fromDTO(objDto);		
 		obj = service.insert(obj);		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+				
 		
-	}*/
+	}
 }
