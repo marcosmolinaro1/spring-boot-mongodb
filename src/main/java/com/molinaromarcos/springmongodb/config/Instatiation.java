bc05b697270f4776cfa54d5f0ca50e7c17dd20b4
marcosmolinaro1 <marcos.molinaro@hotmail.com>
2020-07-13 18:27:36 -0300

    Adding comments on the post

--- src/main/java/com/molinaromarcos/springmongodb/config/Instatiation.java
+++ src/main/java/com/molinaromarcos/springmongodb/config/Instatiation.java
@@ -1,6 +1,7 @@
 package com.molinaromarcos.springmongodb.config;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.molinaromarcos.springmongodb.domain.Post;
 import com.molinaromarcos.springmongodb.domain.User;
 import com.molinaromarcos.springmongodb.dto.AuthorDTO;
+import com.molinaromarcos.springmongodb.dto.CommentDTO;
 import com.molinaromarcos.springmongodb.repository.PostRepository;
 import com.molinaromarcos.springmongodb.repository.UserRepository;
 
@@ -43,6 +45,12 @@ public class Instatiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(java.util.Arrays.asList(c1,c2));
+		post2.getComments().addAll(java.util.Arrays.asList(c3));		
 		
 		postRepository.saveAll(java.util.Arrays.asList(post1, post2));
 		
