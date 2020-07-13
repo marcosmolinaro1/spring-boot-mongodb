61bdb39fb8b14f1986914265d6a6122246104d31
marcosmolinaro1 <marcos.molinaro@hotmail.com>
2020-07-13 15:29:56 -0300

    Creation AuthorDTO

--- src/main/java/com/molinaromarcos/springmongodb/config/Instatiation.java
+++ src/main/java/com/molinaromarcos/springmongodb/config/Instatiation.java
@@ -3,13 +3,13 @@ package com.molinaromarcos.springmongodb.config;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.molinaromarcos.springmongodb.domain.Post;
 import com.molinaromarcos.springmongodb.domain.User;
+import com.molinaromarcos.springmongodb.dto.AuthorDTO;
 import com.molinaromarcos.springmongodb.repository.PostRepository;
 import com.molinaromarcos.springmongodb.repository.UserRepository;
 
@@ -37,10 +37,13 @@ public class Instatiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
 		
 		userRepository.saveAll(java.util.Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(java.util.Arrays.asList(post1, post2));
 		
 	}
